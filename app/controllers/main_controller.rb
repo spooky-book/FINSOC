9fc09307fd6a1dd7cdaea88c83a0ccaba78257fc
Daniel Bu <daniel.bu329@gmail.com>
2017-10-09 20:52:29 +1100

    Cleanup

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,11 @@
 class MainController < ApplicationController
   def index
-    # TODO Clean up show_new_page variable
-    render "main/index", locals: { show_new_page: true }
   end
 
   def events
   end
 
   def sponsorship
-    render "main/sponsorship", locals: { show_new_page: true }
   end
 
   def about_us
@@ -29,11 +26,4 @@ class MainController < ApplicationController
   def hsbc_womens_mentoring
   end
 
-  #def new
-  #  render "main/index", locals: { show_new_page: true }
-  #end
-
-  #def newsponsors
-  #  render "main/sponsorship", locals: { show_new_page: true }
-  #end
 end
