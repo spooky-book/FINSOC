c9fdb3cf43d946ada389b526b82bdae5c212eea7
Tim Xue <xuegot@hotmail.com>
2020-06-07 11:33:04 +1000

    updating all files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Removes error of pending migrations
+  config.active_record.migration_error = false
 end
